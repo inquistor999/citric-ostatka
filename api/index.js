53d5856ed408a34afc7247b03e57d8293fc75d6a
inquistor999 <inquistor999@example.com>
2026-05-14 15:25:23 +0500

    Move static files to public/ and update api path

--- api/index.js
+++ api/index.js
@@ -21,7 +21,7 @@ bot.start((ctx) => {
 // Express server to serve front‑end and handle Excel generation
 const app = express();
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/api/health', (req, res) => res.send('OK'));
 
